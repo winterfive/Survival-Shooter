d4aea2600509faa9ce7217c73e3906dbd61b3b8c
Lee <winterfive@users.noreply.github.com>
2018-05-23 17:06:48 -0400

    Create EnemyMover.cs

---
+++ EnemyMover.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    UnityEngine.AI.NavMeshAgent nav;
+
+
+    void Awake ()
+    {
+        // Find the player transform and assign it
+        player = GameObject.FindGameObjectWithTag ("Player").transform;
+        playerHealth = player.GetComponent <PlayerHealth> ();
+        enemyHealth = GetComponent <EnemyHealth> ();
+        nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+    }
+
+
+    void Update ()
+    {
+        // If the player and the enemy aren't dead, move towards the player
+        if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {
+            nav.SetDestination (player.position);
+        }
+        else
+        {
+            nav.enabled = false;
+        }
+    }
+}
