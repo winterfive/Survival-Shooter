598bbff93f866e6a6b74c54c0f9aa4b3b2e9cad8
Lee <winterfive@users.noreply.github.com>
2018-05-23 17:15:05 -0400

    Added more comments

--- EnemyMovement.cs
+++ EnemyMovement.cs
@@ -1,34 +1,40 @@
 using UnityEngine;
 using System.Collections;
 
-public class EnemyMovement : MonoBehaviour
+namespace CompleteProject
 {
-    Transform player;
-    PlayerHealth playerHealth;
-    EnemyHealth enemyHealth;
-    UnityEngine.AI.NavMeshAgent nav;
-
-
-    void Awake ()
+    public class EnemyMovement : MonoBehaviour
     {
-        // Find the player transform and assign it
-        player = GameObject.FindGameObjectWithTag ("Player").transform;
-        playerHealth = player.GetComponent <PlayerHealth> ();
-        enemyHealth = GetComponent <EnemyHealth> ();
-        nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
-    }
+        Transform player;                       // Reference to the player's position.
+        PlayerHealth playerHealth;              // Reference to the player's health.
+        EnemyHealth enemyHealth;                // Reference to this enemy's health.
+        UnityEngine.AI.NavMeshAgent nav;        // Reference to the nav mesh agent.
 
 
-    void Update ()
-    {
-        // If the player and the enemy aren't dead, move towards the player
-        if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        void Awake ()
         {
-            nav.SetDestination (player.position);
+            // Set up the references.
+            player = GameObject.FindGameObjectWithTag ("Player").transform;
+            playerHealth = player.GetComponent <PlayerHealth> ();
+            enemyHealth = GetComponent <EnemyHealth> ();
+            nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
         }
-        else
+
+
+        void Update ()
         {
-            nav.enabled = false;
+            // If the enemy and the player have health left...
+            if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+            {
+                // ... set the destination of the nav mesh agent to the player.
+                nav.SetDestination (player.position);
+            }
+            // Otherwise...
+            else
+            {
+                // ... disable the nav mesh agent.
+                nav.enabled = false;
+            }
         }
     }
 }
